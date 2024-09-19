0e3644c8f0dc97ed661ee5a327026ec1d04b01ac
Priya sharma <swasti9619@gmail.com>
2024-09-19 21:39:26 +0530

    13 sep assigment commit

---
+++ src/Sept_13_Assignment/calculator.java
@@ -0,0 +1,15 @@
+package Sept_13_Assignment;
+
+public class calculator {
+    public static void main(String[] args) {
+        double a = 34;
+        double b = 10;
+
+        System.out.printf("%f + %f = %f\n",a,b,a+b);
+        System.out.printf("%f - %f = %f\n",a,b,a-b);
+        System.out.printf("%f x %f = %f\n",a,b,a*b);
+        System.out.printf("%f / %f = %f\n",a,b,a/b);
+        System.out.printf("%f mod %f = %f\n",a,b,a%b);
+
+    }
+}
---
+++ src/Sept_13_Assignment/table.java
@@ -0,0 +1,17 @@
+package Sept_13_Assignment;
+
+public class table {
+    public static void main(String[] args) {
+        int num = 10;
+        System.out.printf(num +"x %d = %d\n",1,num*1);
+        System.out.printf(num +"x %d = %d\n",2,num*2);
+        System.out.printf(num +"x %d = %d\n",3,num*3);
+        System.out.printf(num +"x %d = %d\n",4,num*4);
+        System.out.printf(num +"x %d = %d\n",5,num*5);
+        System.out.printf(num +"x %d = %d\n",6,num*6);
+        System.out.printf(num +"x %d = %d\n",7,num*7);
+        System.out.printf(num +"x %d = %d\n",8,num*8);
+        System.out.printf(num +"x %d = %d\n",9,num*9);
+        System.out.printf(num +"x %d = %d\n",10,num*10);
+    }
+}
